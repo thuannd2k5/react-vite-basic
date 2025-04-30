8438ae52e31676e09ef2ffb073f149e9fcb95fc0
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-04-30 09:09:57 +0700

    video 22

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello world & nguyen duc thuan </h1>
+      <h1>Hello world & anh thuan pro </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
