4f91c9cc6adb6922cacbba66adc40d11f4bfa5a1
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-04-30 18:29:05 +0300

    video 29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+  return (
+    <div>
+      <h2>My Component</h2>
+      <p>This is a simple component.</p>
+    </div>
+  );
+};
+export default MyComponent; //xuat ra 1 bien
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,19 @@
+const SecondComponent = () => {
+  return (
+    <div>
+      <h2>Second Component</h2>
+      <p>This is the second component.</p>
+    </div>
+  );
+};
+
+const ThirdComponent = () => {
+  return (
+    <div>
+      <h2>Third Component</h2>
+      <p>This is the Third component.</p>
+    </div>
+  );
+};
+
+export { SecondComponent, ThirdComponent }; //xuat ra nhièu bien
