798a2f46de43ccd3fcbe775b8b184e5fbd1ba223
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-05-01 08:49:40 +0700

    vd30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,16 @@
+//fragment
+
 const MyComponent = () => {
   return (
-    <div>
-      <h2>My Component</h2>
-      <p>This is a simple component.</p>
-    </div>
+    <>
+      <div>
+        <h2>My Component</h2>
+        <p className="child" style={{ color: "red" }}>
+          This is a simple component.
+        </p>
+      </div>
+    </>
   );
 };
+
 export default MyComponent; //xuat ra 1 bien
