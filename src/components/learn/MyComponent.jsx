4a1469cb1e8079dd6945af7277d7e045e5a59b03
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-05-04 16:15:03 +0700

    mvideo 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,21 @@
-//fragment
+//fragment <></> : Fragment
 
 const MyComponent = () => {
+  // const thuan = "thuan";
+  // const thuan = 25;
+  // const thuan = true;
+  // const thuan = undefined;
+  // const thuan = null;
+
+  // const thuan = [1, 2, 3, 4, 5]; // array
+  const thuan = {
+    name: "thuan",
+    age: 18,
+  };
   return (
     <>
       <div>
-        <h2>My Component</h2>
+        <h2>{JSON.stringify(thuan)}</h2>
         <p className="child" style={{ color: "red" }}>
           This is a simple component.
         </p>
